c6374653dc8cbb77a7f264e4a73aaafeac703e0c
eliezer <eliezer.moraes@outlook.com>
2023-06-05 20:55:37 -0300

    Implemented projection - Supplier list by product name

---
+++ backend/src/main/java/com/eliezer/iestoque/projections/SupplierByProductDTO.java
@@ -0,0 +1,10 @@
+package com.eliezer.iestoque.projections;
+
+public interface SupplierByProductDTO {
+
+    Long getId();
+    Integer getCodFornecedor();
+    String getProductCode();
+    String getProductDescription();
+
+}
--- backend/src/main/java/com/eliezer/iestoque/repositories/SupplierRepository.java
+++ backend/src/main/java/com/eliezer/iestoque/repositories/SupplierRepository.java
@@ -1,8 +1,21 @@
 package com.eliezer.iestoque.repositories;
 
 import com.eliezer.iestoque.entities.Supplier;
+import com.eliezer.iestoque.projections.SupplierByProductDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT tb_supplier.id, tb_supplier.cod_fornecedor, tb_product.product_code,
+            tb_product.product_description
+            FROM tb_supplier
+            INNER JOIN tb_product ON tb_supplier.id = tb_product.supplier_id
+            WHERE tb_product.product_description LIKE LOWER(CONCAT('%',:nameProduct,'%'))    
+            """)
+    List<SupplierByProductDTO> searchSupplierByProduct(String nameProduct);
 
 }
