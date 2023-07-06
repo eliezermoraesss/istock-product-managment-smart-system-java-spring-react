a1aa2b3090f47b760f21097a57d4f5c1d5c58c98
Eliezer Moraes Silva <77923494+eliezermoraesss@users.noreply.github.com>
2023-07-06 08:59:33 -0300

    Teste VSCode Web

--- backend/src/main/java/com/eliezer/iestoque/IestoqueApplication.java
+++ backend/src/main/java/com/eliezer/iestoque/IestoqueApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IestoqueApplication {
 
+//teste
 	public static void main(String[] args) {
 		SpringApplication.run(IestoqueApplication.class, args);
 	}
