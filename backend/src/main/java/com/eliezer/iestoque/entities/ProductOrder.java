c4713fdc84de267fc9b858315ed6597296aad45a
eliezer <eliezer.moraes@outlook.com>
2023-07-19 21:18:08 -0300

    ProductOrder getters and setters

--- backend/src/main/java/com/eliezer/iestoque/entities/ProductOrder.java
+++ backend/src/main/java/com/eliezer/iestoque/entities/ProductOrder.java
@@ -7,12 +7,8 @@ import jakarta.persistence.Table;
 import lombok.*;
 
 import java.time.Instant;
+import java.util.Objects;
 
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "tb_request_product")
 public class ProductOrder {
@@ -21,5 +17,45 @@ public class ProductOrder {
     @GeneratedValue
     private Long id;
     private Instant orderDate;
+    
+    public ProductOrder() {    	
+    }
+    
+    public ProductOrder(Long id, Instant orderDate) {
+    	this.id = id;
+    	this.orderDate = orderDate;
+    }
+    
+    public Long getId() {
+    	return id;
+    }
+    
+    public void setId(Long id) {
+    	this.id = id;
+    }
 
+	public Instant getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Instant orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductOrder other = (ProductOrder) obj;
+		return Objects.equals(id, other.id);
+	}
 }
