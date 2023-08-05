ddc603d672ca8e7c3cb5be8e442b93dcd6012424
eliezer <eliezer.moraes@outlook.com>
2023-08-05 17:10:43 -0300

    Criado classe de configuração OpenApiConfiguration para exibir informação de projeto no cabeçalho do Swagger

---
+++ backend/src/main/java/com/eliezer/iestoque/config/OpenApiConfiguration.java
@@ -0,0 +1,27 @@
+package com.eliezer.iestoque.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "iStock API", version = "v1", description = "Documentação da iStock API"))
+public class OpenApiConfiguration {
+
+	@Bean
+	public OpenAPI customOpenApi() {
+		return new OpenAPI().components(new Components())
+				.info(
+					new io.swagger.v3.oas.models.info.Info()
+					.title("iStock API")
+					.version("v1")
+					.license(
+							new License()
+							.name("Apache 2.0")
+							.url("http://springdoc.org")));
+	}
+}
